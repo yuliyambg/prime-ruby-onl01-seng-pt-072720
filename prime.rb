400cc091e453d81baa0cbb46f788919f323248cf
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-31 02:02:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,9 @@
 def prime?(num)
   return false if num < 2
 
-  (2..num/2).none?{|i| num % i == 0}
+  (2..num).none?{|i| num % i == 0}
 end
 
-def prime?(10)
-  return false if 10 < 2
-  
-  (2..5).none?{|5|  10 % 5 == 0 }
+
 
 
